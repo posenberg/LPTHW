9f02e200351c4c0ffaa29a6c54333524f6b0c3b3
Melinda Po <mellypo@gmail.com>
2014-03-12 09:52:56 -0700

    Parameters Unpacking Variables

---
+++ ex13.py
@@ -0,0 +1,13 @@
+from sys import argv #import modules or "libraries", so the system module has  an argument variable
+
+script, first, second, third= argv
+
+print "The script is called: ", script
+print "Your first variable is: ", first
+print "Your second variable is: ", second
+print "Your third variable is: ", third
+
+
+#  Exercise 13: Paramater, Unpacking, Variables
+# When you run the python script in terminal it should be like this: python ex13.py value1 value2 value3
+# these argument variables should be accepted due to the variables    script, first, second, third = argv 
